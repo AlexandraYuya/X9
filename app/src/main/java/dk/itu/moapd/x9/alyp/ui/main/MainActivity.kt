d842ea2099268cd37ff9d4d4f873870e549d8be7
AlexandraYuya <alyp@itu.dk>
2026-02-08 16:14:07 +0100

    init commit

---
+++ app/src/main/java/dk/itu/moapd/x9/alyp/ui/main/MainActivity.kt
@@ -0,0 +1,28 @@
+package dk.itu.moapd.x9.alyp.ui.main
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import dk.itu.moapd.x9.alyp.R
+import dk.itu.moapd.x9.alyp.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.textViewHello.text = "Hello Alexandra"
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
