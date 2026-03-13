7f6774d8c93b4c5b979fc8b7f897dc206bd08ae3
AlexandraYuya <alyp@itu.dk>
2026-03-13 10:13:04 +0100

    add default coroutine dependencies, minor viewmodel refactoring

--- app/src/main/java/dk/itu/moapd/x9/alyp/ReportViewModel.kt
+++ app/src/main/java/dk/itu/moapd/x9/alyp/ReportViewModel.kt
@@ -1,16 +1,43 @@
 package dk.itu.moapd.x9.alyp
 
+import android.util.Log
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
 import java.util.Date
 import java.util.UUID
 
+private const val TAG = "ReportViewModel"
 class ReportViewModel : ViewModel() {
     val reports: MutableLiveData<List<Report>> by lazy {
         MutableLiveData<List<Report>>()
     }
 
     init {
+        Log.d(TAG, "init starting")
+        viewModelScope.launch {
+            Log.d(TAG, "coroutine launched")
+//            delay(5000)
+//            val initial = List(1) { i ->
+//                Report(
+//                    id = UUID.randomUUID(),
+//                    title = "Report #$i",
+//                    location = "ITU",
+//                    date = Date(),
+//                    type = "stuDying",
+//                    description = "lorem ipsum",
+//                    severity = "moderate"
+//                )
+//            }
+            reports.value = loadReports()
+            Log.d(TAG, "Loading reports finished")
+        }
+    }
+    suspend fun loadReports(): List<Report> {
+        val result = mutableListOf<Report>()
+        delay(5000)
         val initial = List(1) { i ->
             Report(
                 id = UUID.randomUUID(),
@@ -22,9 +49,9 @@ class ReportViewModel : ViewModel() {
                 severity = "moderate"
             )
         }
-        reports.value = initial
+        result += initial
+        return result
     }
-
     fun addReport(report: Report) {
         val current = reports.value.orEmpty()
         reports.value = current + report
